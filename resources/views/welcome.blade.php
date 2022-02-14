a1fb4e846941d3adae3e3572a111b18cb8ffb932
= <=>
2022-02-14 19:09:50 +0700

    Update Kelas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </head>
         <body class="antialiased">
             <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white"> Lia Puspita Dewi</h1></div>
+                <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white"> Lia Puspita Dewi TI-2E</h1></div>
         </div>
             </body>
 </html>
